35d9b07508d0c21d2c4b326a0797725c76bec800
aaronjamesreynolds <reynolaa@oregonstate.edu>
2018-06-23 10:45:34 -0700

    Added a second class to input module, edited test_input, and created a test module.

--- input/read.py
+++ input/read.py
@@ -4,7 +4,20 @@ import yaml
 
 """Reads in nuclear data from a yaml input file"""
 
-class InputData(yaml.YAMLObject):
+
+class Input:
+
+    def __init__(self, input_file_name):
+
+        with open(input_file_name, 'r') as stream:
+            try:
+                self.data = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+
+class InputDataYAML(yaml.YAMLObject):
+
     yaml_tag = u'!InputData'
 
     def __init__(self, sig_t, sig_s, sig_f, nu, cells, material):
@@ -17,6 +30,11 @@ class InputData(yaml.YAMLObject):
 
     def __repr__(self):
         return "%s(sig_t = %r, sig_s=%r, sig_f=%r, nu=%r, cells = %r, material = %r)" % (self.__class__.__name__,
-                                                                                         self.sig_t, self.sig_s,                                                                                     self.sig_f, self.nu,
+                                                                                         self.sig_t, self.sig_s,
+                                                                                         self.sig_f, self.nu,
                                                                                          self.cells, self.material)
+
 if __name__ == '__main__':
+
+    test = Input("test_input.yaml")
+    print test.data
